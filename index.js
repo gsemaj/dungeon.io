95743cfac54fae7ee6284b961b286ef02509d7dd
gsemaj <gentsemaj@live.com>
2021-03-08 22:20:33 -0500

    Serve static resources properly and add client script

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 console.log('Starting dungeon.io server...');
 
 const config = require('config');
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 
+// Serve client index page
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
 });
 
+// Serve static client resources, redirecting root directory to client directory
+app.use(express.static('client'));
+
+// Load config fields
 const port = config.has('web.port') ? config.get('web.port') : 80;
+
 http.listen(port, () => {
     console.log('Server listening on *:' + port);
 });
